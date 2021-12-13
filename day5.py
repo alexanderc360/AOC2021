bfbd7170aa7bb00cb6f4787f256460cebcdf93a9
Alexander Cohen <cohenah@clarkson.edu>
2021-12-12 21:00:49 -0500

    finished day 5

--- day5.py
+++ day5.py
@@ -1,3 +1,39 @@
+def draw(start, end, grid):
+	#print(start, end)
+	if start[0] == end[0]:  # verticle
+		if start[1] > end[1]: 
+			for i in range(end[1], start[1] + 1, 1):
+				grid[i][start[0]] += 1
+		else:
+			for i in range(start[1], end[1] + 1, 1):
+				grid[i][end[0]] += 1
+	elif start[1] == end[1]:  # horizontal 
+		if start[0] > end[0]:
+			for i in range(end[0], start[0] + 1, 1):
+				grid[start[1]][i] += 1
+		else:
+			for i in range(start[0], end[0] + 1, 1):
+				grid[end[1]][i] += 1
+				# diagnol
+	else:	
+		if start[0] > end[0]:
+			if start[1] > end[1]:
+				for i in range(start[0] - end[0] + 1):
+					grid[start[1] - i][start[0] - i] += 1
+			if start[1] < end[1]:
+				for i in range(start[0] - end[0] + 1):
+					grid[start[1] + i][start[0] - i] += 1
+		elif start[0] < end[0]:
+                	if start[1] > end[1]:
+                        	for i in range(end[0] - start[0] + 1):
+                                	grid[start[1] - i][start[0] + i] += 1
+                	if start[1] < end[1]:
+                        	for i in range(end[0] - start[0] + 1):
+                                	grid[start[1] + i][start[0] + i] += 1
+
+	return grid
+
+
 file = open("input-2021-5.txt")
 
 
@@ -29,6 +65,26 @@ for i in range(len(startPoints)):
 #print(startPoints)
 size = demention(startPoints, endPoints)
 
-grid = [[0]*size]*size
+grid = []
+for i in range(size + 1):
+	buff = []
+	for j in range(size + 1):
+		buff.append(0)
+		#buff.append((i, j))
+	grid.append(buff)
 
-print(grid)
+#print(startPoints)
+
+for i in range(len(startPoints)):
+	grid = draw(startPoints[i], endPoints[i], grid)
+
+#for i in grid:
+#	print(i)
+
+count = 0
+for i in range(len(grid)):
+	for j in range(len(grid[0])):
+		if grid[i][j] > 1:
+			count += 1
+
+print(count)
