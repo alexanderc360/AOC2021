0588ec5ab9b5dcbcb77267dbf2622884d1075de1
Alexander Cohen <cohenah@clarkson.edu>
2021-12-14 14:38:47 -0500

    finished day 13

--- day13.py
+++ day13.py
@@ -4,7 +4,6 @@ def genNewGrid(axis, val, grid):
     y = len(grid)
 
     if axis == 'x':
-        print(val)
         for i in range(y):
             buff = []
             for j in range(val):
@@ -14,7 +13,6 @@ def genNewGrid(axis, val, grid):
                     buff.append('.')
             newGrid.append(buff)
     else:
-        print(val)
         for i in range(val):
             buff = []
             for j in range(x):
@@ -24,15 +22,23 @@ def genNewGrid(axis, val, grid):
                     buff.append('.')
             newGrid.append(buff)
 
-    #for i in newGrid:
-     #   print(i)
     return newGrid
 
 
 def fold(axis, val, grid):
-    newGrid = genNewGrid(axis, val, grid)
+	newGrid = genNewGrid(axis, val, grid)
+	if axis == 'x':
+		for i in range(len(grid)):
+                        for j in range(val + 1, len(grid[0]), 1):
+                                if grid[i][j] == '#' and grid[i][2 * val -j] != '#':
+                                        newGrid[i][2 * val -j] = '#'
+	else:
+		for i in range(val + 1, len(grid), 1):
+			for j in range(len(grid[0])):
+				if grid[i][j] == '#' and grid[2 * val - i][j] != '#':
+					newGrid[2 * val - i][j] = '#'
 
-    return newGrid
+	return newGrid
 
 
 file = open("input-2021-13.txt")
@@ -63,18 +69,19 @@ for i in range(y):
     paper.append(buff)
 
 for p in points:
-    for i in range(y):
-        for j in range(x):
-            if j == p[0] and i == p[1]:
-                paper[i][j] = '#'
+	paper[p[1]][p[0]] = '#'
 
 for line in file:
-    print(line)
-    axis = line[line.index('=') - 1:line.index('=')]
-    val = int(line[line.index('=') + 1:])
-    paper = fold(axis,val, paper)
-    #paper = genNewGrid(axis,val,paper)
+	axis = line[line.index('=') - 1:line.index('=')]
+	val = int(line[line.index('=') + 1:])
+	paper = fold(axis,val, paper)
+	count = 0
+	for i in paper:
+		for j in i:
+	        	if j == '#':
+	               		count += 1
+	print(count)
 
-print()
 for p in paper:
     print(p)
+
